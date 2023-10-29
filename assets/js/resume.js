a763adfa374c77a84c64438006460eceeb452fb5
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2023-10-29 19:19:26 +0530

    Update resume.js

---
+++ assets/js/resume.js
@@ -0,0 +1,61 @@
+// regex for validation
+const strRegex = /^[a-zA-Z\s]*$/; // containing only letters
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+/* supports following number formats - (123) 456-7890, (123)456-7890, 123-456-7890, 123.456.7890, 1234567890, +31636363634, 075-63546725 */
+const digitRegex = /^\d+$/;
+
+const mainForm = document.getElementById('cv-form');
+const validType = {
+    TEXT: 'text',
+    TEXT_EMP: 'text_emp',
+    EMAIL: 'email',
+    DIGIT: 'digit',
+    PHONENO: 'phoneno',
+    ANY: 'any',
+}
+
+// user inputs elements
+let firstnameElem = mainForm.firstname,
+    middlenameElem = mainForm.middlename,
+    lastnameElem = mainForm.lastname,
+    imageElem = mainForm.image,
+    designationElem = mainForm.designation,
+    addressElem = mainForm.address,
+    emailElem = mainForm.email,
+    phonenoElem = mainForm.phoneno,
+    summaryElem = mainForm.summary;
+
+// display elements
+let nameDsp = document.getElementById('fullname_dsp'),
+    imageDsp = document.getElementById('image_dsp'),
+    phonenoDsp = document.getElementById('phoneno_dsp'),
+    emailDsp = document.getElementById('email_dsp'),
+    addressDsp = document.getElementById('address_dsp'),
+    designationDsp = document.getElementById('designation_dsp'),
+    summaryDsp = document.getElementById('summary_dsp'),
+    projectsDsp = document.getElementById('projects_dsp'),
+    achievementsDsp = document.getElementById('achievements_dsp'),
+    skillsDsp = document.getElementById('skills_dsp'),
+    educationsDsp = document.getElementById('educations_dsp'),
+    experiencesDsp = document.getElementById('experiences_dsp');
+
+// first value is for the attributes and second one passes the nodelists
+const fetchValues = (attrs, ...nodeLists) => {
+    let elemsAttrsCount = nodeLists.length;
+    let elemsDataCount = nodeLists[0].length;
+    let tempDataArr = [];
+
+    // first loop deals with the no of repeaters value
+    for (let i = 0; i < elemsDataCount; i++) {
+        let dataObj = {}; // creating an empty object to fill the data
+        // second loop fetches the data for each repeaters value or attributes 
+        for (let j = 0; j < elemsAttrsCount; j++) {
+            // setting the key name for the object and fill it with data
+            dataObj[`${attrs[j]}`] = nodeLists[j][i].value;
+        }
+        tempDataArr.push(dataObj);
+    }
+
+    return tempDataArr;
+}
