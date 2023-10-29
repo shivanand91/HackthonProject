654d051c667f8b0b8ffbd77d862581ea2a451516
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2023-10-29 10:37:59 +0530

    Delete script.js

--- assets/script.js
+++
@@ -1,9 +0,0 @@
-const mobile_nav = document.querySelector(".hamburger-menu");
-
-const nav_header = document.querySelector(".header");
-
-
-const toggleNavbar = () => {
-    nav_header.classList.toggle("active");
-};
-mobile_nav.addEventListener("click", () => toggleNavbar());
